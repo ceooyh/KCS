d6104bd1547edf021e2d0ebb1a947feea4bbbc2c
cgd0318 <cgd0318@naver.com>
2021-03-03 12:38:35 +0900

    예약 관리 날짜 정보 추가 -희원

--- src/main/java/kcs/controller/BookController.java
+++ src/main/java/kcs/controller/BookController.java
@@ -251,6 +251,12 @@ public class BookController {
  				// 사업자 번호와 일치하는 예약 목록
  				ArrayList<BookDTO> list = service.getManageBookList(bizrno); 
  				request.setAttribute("list", list);
+ 				
+ 				// 날짜 정보
+ 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+ 				Calendar cal = Calendar.getInstance();
+ 				String today = sdf.format(cal.getTime());
+ 				request.setAttribute("today", today);
  				return "book/manage_book_list";
  			}
 		} catch (IOException e) {
